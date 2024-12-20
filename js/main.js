62d2063366bdc8828fb21606bbaa03eda1f1fd8e
Roma <rtereshchenko17@gmail.com>
2024-12-20 14:36:57 +0300

    Initial implementation of swiper-outer

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-outer', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 4,
+    spaceBetween: 80,
+});
